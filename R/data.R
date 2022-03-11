dc1e1a6eef00bca0ddab0d5d0c326e67439b79fd
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-03-11 09:20:53 +0100

    Single trial example data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' A single trial samples recorded at 500 Hz.
+#'
+#' A single trial samples recorded at 500 Hz.
+#'
+#' @format A data frame with 1006 rows and 2 variables:
+#' \describe{
+#'   \item{x}{X coordinate in degrees of visual angle.}
+#'   \item{y}{Y coordinate in degrees of visual angle.}
+#'   ...
+#' }
+"single_trial"
