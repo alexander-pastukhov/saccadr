4da6c6891f5b550407be464e8890502b8d978b05
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-03-11 09:54:57 +0100

    Package docs

---
+++ R/package.R
@@ -0,0 +1,13 @@
+#' saccadr
+#'
+#' @description Extract (Micro)Saccades From Gaze Samples.
+#'
+#' @docType package
+#' @import Rcpp
+#' @import RcppProgress
+#' @importFrom Rcpp evalCpp
+#' @useDynLib saccadr
+#' @name saccadr
+NULL
+
+#  usethis::use_git_config(user.name = "Alexander (Sasha) Pastukhov", user.email = "pastukhov.alexander@gmail.com")
