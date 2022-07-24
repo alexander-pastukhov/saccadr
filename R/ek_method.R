11a9c5b9e78fc31e76055ef7d6aa3bb591dbf3fb
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-07-24 13:11:57 +0200

    Rename velx/vely to x/y

--- R/ek_method.R
+++ R/ek_method.R
@@ -1,13 +1,18 @@
 #' Extract microsaccades using an algorithm proposed by Engbert and Kliegl (2003)
 #'
-#' @description Extract microsaccades using an algorithm proposed by Engbert and
-#' Kliegl (2003).
+#' @description Marks out samples that belong to saccades using an algorithm
+#' proposed by Engbert and Kliegl (2003). Do not call this function directly,
+#' instead use the common interface via \code{\link{extract_microsaccades}} passing
+#' additional parameters (\code{velocity_threshold}, \code{sd_fun},
+#' \code{minimal_duration_ms}, \code{minimal_separation_ms}) via its \code{options}
+#' parameter.
 #'
-#' @param x Gaze x coordinate, arbitrary units as threshold velocity is computed in units of standard deviation.
-#' @param y Gaze x coordinate, arbitrary units as threshold velocity is computed in units of standard deviation.
+#' @param x Gaze x coordinate, _arbitrary units_ as threshold velocity is computed in units of standard deviation.
+#' @param y Gaze x coordinate, _arbitrary units_ as threshold velocity is computed in units of standard deviation.
+#' @param vel Velocity data.frame with columns \code{x}, \code{y}, \code{amp}.
+#' @param acc Acceleration data.frame with columns \code{x}, \code{y}, \code{amp}.
 #' @param sample_rate Sample rate of the recording in Hz.
 #' @param trial Trial id. Trial borders are respected when computing velocity.
-#' All samples are assumed to belong to the same trial, if \code{trial = NULL}.
 #' @param velocity_time_window Time span in milliseconds relative to the sample
 #' that is used  to compute the velocity. Defaults to 20 ms.
 #' @param velocity_threshold Velocity threshold for microsaccade detection
@@ -21,16 +26,18 @@
 #' Two microsaccades that a separated by a smaller time gap are merged into a single
 #' microsaccade. Defaults to 0 ms.
 #'
-#' @return logical vector marking samples that belong to a saccade.
+#' @return logical vector marking samples that belong to saccades
+#' @seealso \code{\link{extract_microsaccades}}
 #' @export
 #'
 #' @examples
+#' # do not run this function directly, use extract_microsaccades
 extract_ms_ek <- function(x,
                           y,
                           vel,
                           acc,
                           sample_rate,
-                          trial = NULL,
+                          trial,
                           velocity_threshold = 6,
                           sd_fun = sd_via_median_estimator,
                           minimal_duration_ms = 12,
@@ -42,23 +49,23 @@ extract_ms_ek <- function(x,
   minimal_duration_in_samples <- ceiling(minimal_duration_ms / delta_t_ms)
 
   # standard deviation (by default via median estimator as per formula #2)
-  sigma_xy <- apply(vel[, c("velx", "vely")], MARGIN = 2, FUN = sd_fun, na.rm = TRUE)
+  sigma_xy <- apply(vel[, c("x", "y")], MARGIN = 2, FUN = sd_fun, na.rm = TRUE)
   
   # velocity threshold, formula #3
   vel_threshold <- sigma_xy * velocity_threshold
 
   # computing normalized velocity in the units of the threshold
-  vel_norm <- sqrt((vel[['velx']]/ vel_threshold[1])^2 + (vel[['vely']] / vel_threshold[2])^2)
+  vel_norm <- sqrt((vel[['x']]/ vel_threshold[1])^2 + (vel[['y']] / vel_threshold[2])^2)
 
   # marking out consecutive periods of high (above threshold) velocity
   thresholded_periods <- rle(vel_norm > 1.0)
   
   # labeling potential saccade samples: either super-threshold or brief sub-threshold periods
   thresholded_periods$values <- (thresholded_periods$values == TRUE) |  # 1. super-threshold
-                                (thresholded_periods$values == FALSE &  # 2. sub-threshold
-                                 lead(thresholded_periods$values) == TRUE & # surrounded by super-threshold on both sides
-                                 lag(thresholded_periods$values) == TRUE & 
-                                 thresholded_periods$lengths <= minimal_separation_in_samples) # shorter then minimal fixation
+                                (thresholded_periods$values == FALSE &  # 2. or sub-threshold
+                                 lead(thresholded_periods$values) == TRUE & # but surrounded by super-threshold 
+                                 lag(thresholded_periods$values) == TRUE &  # on both sides
+                                 thresholded_periods$lengths <= minimal_separation_in_samples) # and shorter then minimal fixation
   marked_samples <- inverse.rle(thresholded_periods)
   
   # retaining only potential saccades that are longer than minimal required duration
