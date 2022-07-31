13dc99c9f66416a11c1c7a9b3336024ea5e54362
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-07-31 20:35:22 +0200

    code compiles

--- src/compute_velocity_ek.cpp
+++ src/compute_velocity_ek.cpp
@@ -1,4 +1,3 @@
-
 #include <Rcpp.h>
 using namespace Rcpp;
 
@@ -46,46 +45,40 @@ NumericVector compute_velocity_ek(NumericVector x, IntegerVector trial, int time
 }
 
 
-
-
-#include <Rcpp.h>
-using namespace Rcpp;
-
-
 //' Compute velocity via Engbert & Kliegl (2003) algorithm.
 //'
 //' @description Compute velocity via Engbert & Kliegl (2003) algorithm.
 //' See formula #1 in the manuscript.
-//' 
+//'
 //' @param x Vector of coordinates
 //' @param trial Vector with trial labels, so that velocity is computed only within trials.
 //' @param time_window_in_samples Width of window for velocity computation in samples.
 //' @param delta_t Duration of a single frame (1 / sampling rate).
-//' 
+//'
 //' @return Velocity vector
 //' @export
 // [[Rcpp::export]]
 NumericVector compute_velocity_via_ek(NumericVector x, IntegerVector trial, int time_window_in_samples, float delta_t){
   // maximal half span of window, so it goes from imiddle +- half_span
   int max_half_span = (time_window_in_samples - 1) / 2;
-  
+
   // Compute total weight for velocity normalization.
-  NumericVector distance_weight(max_half_span, 0);
+  NumericVector weight(max_half_span, 0.0);
   int span_sum = 0;
   for(int span = 1; span <= max_half_span; span ++){
     span_sum += span;
-    distance_weight[span - 1] = span_sum * 2 * delta_t;
+    weight[span - 1] = span_sum * 2 * delta_t;
   }
 
   // Allocate velocity vector
-  NumericVector v(x.size(), 0);
-  
+  NumericVector v(x.size(), 0.0);
+
   for(int imiddle = 1; imiddle < x.size() - 1; imiddle++){
     // finding applicable filter width starting with the widest one
     int half_span = max_half_span;
     int istart = imiddle - half_span;
     int iend = imiddle + half_span;
-    
+
     // shrink until window is valid or it is zero
     while(((istart < 0) ||                      // start must not be outside of the vector
            (iend >= x.size()) ||                // end must not be outside of the vector
@@ -96,18 +89,18 @@ NumericVector compute_velocity_via_ek(NumericVector x, IntegerVector trial, int 
       istart = imiddle - half_span;
       iend  = imiddle + half_span;
     }
-    
+
     if (half_span > 0){
       float current_v = 0;
-      
+
       // negative side
       for(int i = istart; i < imiddle; i++) current_v -= x[i];
-      
+
       // positive side
       for(int i = imiddle + 1; i <= iend; i++) current_v += x[i];
-      
+
       // normalize
-      v[imiddle] = current_v / distance_weight[half_span - 1];
+      v[imiddle] = current_v / weight[half_span - 1];
     }
   }
 
