6d6377dfd715e3cc3cce39c35c18f82db1432a72
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-07-31 20:55:00 +0200

    velocity computation better handling edge cases

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -10,20 +10,6 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
-// compute_velocity_ek
-NumericVector compute_velocity_ek(NumericVector x, IntegerVector trial, int time_window_in_samples, float delta_t);
-RcppExport SEXP _saccadr_compute_velocity_ek(SEXP xSEXP, SEXP trialSEXP, SEXP time_window_in_samplesSEXP, SEXP delta_tSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP);
-    Rcpp::traits::input_parameter< IntegerVector >::type trial(trialSEXP);
-    Rcpp::traits::input_parameter< int >::type time_window_in_samples(time_window_in_samplesSEXP);
-    Rcpp::traits::input_parameter< float >::type delta_t(delta_tSEXP);
-    rcpp_result_gen = Rcpp::wrap(compute_velocity_ek(x, trial, time_window_in_samples, delta_t));
-    return rcpp_result_gen;
-END_RCPP
-}
 // compute_velocity_via_ek
 NumericVector compute_velocity_via_ek(NumericVector x, IntegerVector trial, int time_window_in_samples, float delta_t);
 RcppExport SEXP _saccadr_compute_velocity_via_ek(SEXP xSEXP, SEXP trialSEXP, SEXP time_window_in_samplesSEXP, SEXP delta_tSEXP) {
@@ -40,7 +26,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_saccadr_compute_velocity_ek", (DL_FUNC) &_saccadr_compute_velocity_ek, 4},
     {"_saccadr_compute_velocity_via_ek", (DL_FUNC) &_saccadr_compute_velocity_via_ek, 4},
     {NULL, NULL, 0}
 };
