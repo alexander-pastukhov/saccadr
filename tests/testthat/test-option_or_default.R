3225246be575fdaf38e147f25f90d791caa88d48
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2022-09-18 17:57:41 +0200

    submisson to joss

--- tests/testthat/test-option_or_default.R
+++
@@ -1,7 +0,0 @@
-test_that("option_of_default works", {
-  # value from list
-  expect_equal(option_or_default(list("A" = 25), "A", 20), 25)
-  
-  # default value
-  expect_equal(option_or_default(list("A" = 25), "B", 20), 20)
-})
--- tests/testthat/test-sd_via_median_estimator.R
+++
@@ -1,7 +0,0 @@
-test_that("sd_via_median_estimator works", {
-  # scalar
-  expect_equal(length(sd_via_median_estimator(rnorm(n = 100))), 1)
-  
-  # positive
-  expect_true(sd_via_median_estimator(rnorm(n = 100)) > 0)
-})
