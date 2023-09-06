c8badfb803a51ac4a0cc788cfa061322eb9b8ef4
Alexander (Sasha) Pastukhov <pastukhov.alexander@gmail.com>
2023-09-06 14:35:18 +0200

    Same structure as in dplyr with "_PACKAGE_ sentinel

---
+++ R/saccadr.R
@@ -0,0 +1,9 @@
+
+## usethis namespace: start
+#' @description Extract (Micro)Saccades From Gaze Samples.
+#' @keywords internal
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib saccadr
+## usethis namespace: end
+"_PACKAGE"
